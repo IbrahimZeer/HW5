ad415053d9b6cc3c259b112676f1c72704e38c00
ibraheem Zeer <211400@ppu.edu.ps>
2024-02-05 14:59:06 +0200

    Only remove didn't finish

---
+++ main.js
@@ -0,0 +1,18 @@
+const addBtn = document.querySelector(".form");
+const htmlList = document.querySelector(".card .lists");
+let list = [];
+addBtn.onsubmit = (event) => {
+  event.preventDefault();
+  const elements = event.target.elements[0].value;
+  list += `
+    <div class="list" onclick="addClass(this)">
+      <input type="checkbox"/>
+      <span>${elements}</span>
+    </div>
+  `;
+  htmlList.innerHTML = list;
+};
+
+addClass = (c) => {
+  c.classList.add("clicked");
+};
